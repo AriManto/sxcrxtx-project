6f0af1ffe9f6c406986d2e2666ed3afc01567736
AriManto <37150326+AriManto@users.noreply.github.com>
2020-09-24 00:03:40 -0300

    Added brake when pressing spacebar. Updated gitignore

--- sxcrxtx-project/Assets/Scripts/Classes/Player.cs
+++ sxcrxtx-project/Assets/Scripts/Classes/Player.cs
@@ -8,6 +8,9 @@ namespace Assets.Scripts.Classes
 {
     public class Player : MonoBehaviour, IEntity
     {
+        #region Properties and attributes
+        public string Name;
+        /* --- HP STUFF --- */
         public HealthSystem Health { get; set; }
 
         public float StartingHitpoints { get { return _StartingHitpoints; } set { _StartingHitpoints = value; } }
@@ -15,19 +18,26 @@ namespace Assets.Scripts.Classes
 
         public Text HpIndicator; // Reference to the HP indicator for the player
 
-        public string Name;
+        /* --- MOVEMENT STUFF --- */
         //public PlayerMovement Movement;
-        public float speed = 0.2f;
-        public float maxVelocity = 3.5f;
+        // public float maxVelocity = 3.5f;
+        public float speed = 38000f;
         public float rotationInterpolation = 0.4f;
+        private KeyCode brakingKeyCode = KeyCode.Space;
+        private float defaultLinearDrag = 0.8f;
+        public float brakingLinearDrag = 4f;
+        public bool isBraking = false;
         private Rigidbody2D rb;
         [HideInInspector]
         public bool isMoving;
         Vector2 input;
         float facingAngle;
+
+
+        /* --- DEBUG TOGGLES  --- */
         public bool _debugHP = false;
         public bool _debugVelocity = false;
-
+        #endregion
         public void Start()
         {
             Health = gameObject.AddComponent<HealthSystem>();
@@ -37,23 +47,23 @@ namespace Assets.Scripts.Classes
         }
 
         // Update is called once per frame
-        public void Update()
+        private void FixedUpdate()
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
             isMoving = (input.x != 0 || input.y != 0) ? true : false; // Ternary operator, it's a fix to stop player from rotating to initial position
+            isBraking = Input.GetKey(brakingKeyCode);
             //TODO: move this to an event in healthSystem (like "OnHitpointsChanged")
             HpIndicator.text = "HP: " + Health.Hitpoints;
-
             if (_debugHP) Debug.Log("HP:" + this.Health.Hitpoints);
-        }
-        private void FixedUpdate()
-        {
+
+
             rb.AddForce(input * speed * Time.deltaTime, ForceMode2D.Force);
-           /* // Cap maximum velocity [not needed]
-            * if (rb.velocity.sqrMagnitude != maxVelocity * maxVelocity)
-                rb.velocity = rb.velocity.normalized * maxVelocity;*/
+                                   /* // Cap maximum velocity [not needed]
+                                    * if (rb.velocity.sqrMagnitude != maxVelocity * maxVelocity)
+                                        rb.velocity = rb.velocity.normalized * maxVelocity;*/
             if (isMoving) GetRotation();
+            rb.drag = isBraking ? brakingLinearDrag : defaultLinearDrag; // Speed down while holding SPACE KEY
             if (_debugVelocity) Debug.Log(rb.velocity.magnitude);
         }
         void GetRotation()
@@ -76,7 +86,7 @@ namespace Assets.Scripts.Classes
             {
                 Enemy enemy = collision.collider.gameObject.GetComponent<Enemy>();
                 Health.RemoveHitpoints(enemy.CollisionDamage);
-                // Add invincibility for 1 sec (with fade effect) - invincibility timer, invincible status. setTimeout?
+                // TODO: Add invincibility for 1 sec (with fade effect) - invincibility timer, invincible status. setTimeout?
                 CheckAliveState();  // This should go into HealthSystem to handle each entity dead state
 
                 //enemy.gameObject.SetActive(false); // Should be a Destroy or Dispose? Do damage
@@ -87,6 +97,7 @@ namespace Assets.Scripts.Classes
         {
             if (!Health.IsAlive)
             {
+                // Do stuff when ded
                 Debug.Log("U'RE DED M8");
             }
         }
